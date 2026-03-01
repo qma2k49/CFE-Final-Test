8858a01a094cee70abf994672b249aad0289b986
qma2k49 <qma2k49@gmail.com>
2026-03-01 23:22:26 +0700

    Update tính năng tính giờ

---
+++ assets/js/setTime.js
@@ -0,0 +1,20 @@
+// Chạy thời gian
+const btnStart = document.querySelector("#start-button");
+btnStart.addEventListener("click", () => {
+    const timer = setInterval(() => {
+        const minute = document.querySelector("#minute");
+        const second = document.querySelector("#second");
+
+        let minuteValue = parseInt(minute.textContent);
+        let secondValue = parseInt(second.textContent);
+
+        secondValue++;
+        if (secondValue > 59) {
+            secondValue = 0;
+            minuteValue++;
+        }
+
+        second.innerHTML = secondValue >= 10 ? `${secondValue}` : `0${secondValue}`;
+        minute.innerHTML = minuteValue >= 10 ? `${minuteValue}` : `0${minuteValue}`;
+    }, 1000);
+});
